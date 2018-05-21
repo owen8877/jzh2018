b64aa374dec650f6e2eabfb360c84577ad434af4
owen8877 <owentoget@yeah.net>
2018-05-21 08:35:26 +0800

    Modified score.

--- model/douhu/score.m
+++ model/douhu/score.m
@@ -4,10 +4,12 @@ clc; clear
 figure
 s1 = subplot(1, 2, 1);
 load data/param_h-reddit.mat
-engageness = 1:0.01:1.1;
+engageness = 0.5:0.01:1;
 balance_alpha = [1, 1.25, 1.5, 1.75, 2];
 hold(s1, 'on')
 title('Reddit algorithm')
+xlabel('x')
+ylabel('score')
 for alpha = balance_alpha
     scores = arrayfun(@(x) scoring(x, posts_h, alpha), engageness);
     plot(engageness, scores)
@@ -16,10 +18,12 @@ legend(cellfun(@(f) num2str(f), mat2cell(balance_alpha', ones(numel(balance_alph
 
 s2 = subplot(1, 2, 2);
 load data/param_h-hackerNews.mat
-engageness = 1:0.01:1.1;
+engageness = 0.5:0.01:1;
 balance_alpha = [1, 1.25, 1.5, 1.75, 2];
 hold(s2, 'on')
 title('HackerNews Algorithm')
+xlabel('x')
+ylabel('score')
 for alpha = balance_alpha
     scores = arrayfun(@(x) scoring(x, posts_h, alpha), engageness);
     plot(engageness, scores)
